5737eee4c6cf20f05705e16f537632e134e0ca6c
Ed Page <eopage@gmail.com>
2022-07-18 12:43:27 -0500

    refactor(combine): Match grammar order

--- examples/combine-app/app.rs
+++ examples/combine-app/app.rs
@@ -29,46 +29,55 @@ where
     Array(Vec<Value<S>>),
 }
 
-fn lex<'a, P>(p: P) -> impl Parser<Input = P::Input, Output = P::Output>
-where
-    P: Parser,
-    P::Input: RangeStream<Item = u8, Range = &'a [u8]>,
-    <P::Input as StreamOnce>::Error: ParseError<
-        <P::Input as StreamOnce>::Item,
-        <P::Input as StreamOnce>::Range,
-        <P::Input as StreamOnce>::Position,
-    >,
-{
-    no_partial(p.skip(range::take_while(|b| {
-        b == b' ' || b == b'\t' || b == b'\r' || b == b'\n'
-    })))
-}
-
-fn digits<'a, I>() -> impl Parser<Input = I, Output = &'a [u8]> + 'a
+#[inline(always)]
+fn json_value<'a, I>() -> impl Parser<Input = I, Output = Value<&'a str>> + 'a
 where
     I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    range::take_while1(|b| b >= b'0' && b <= b'9')
+    spaces().with(json_value_())
 }
 
-fn number<'a, I>() -> impl Parser<Input = I, Output = f64> + 'a
+// We need to use `parser!` to break the recursive use of `value` to prevent the returned parser
+// from containing itself
+parser! {
+    #[inline(always)]
+    fn json_value_['a, I]()(I) -> Value<&'a str>
+        where [ I: RangeStream<Item = u8, Range = &'a [u8]> + 'a ]
+    {
+        choice((
+            json_string().map(Value::String),
+            object().map(Value::Object),
+            array().map(Value::Array),
+            number().map(Value::Number),
+            lex(range::range(&b"false"[..]).map(|_| Value::Bool(false))),
+            lex(range::range(&b"true"[..]).map(|_| Value::Bool(true))),
+            lex(range::range(&b"null"[..]).map(|_| Value::Null)),
+        ))
+    }
+}
+
+fn object<'a, I>() -> impl Parser<Input = I, Output = HashMap<&'a str, Value<&'a str>>>
 where
     I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    no_partial(
-        lex(range::recognize(no_partial((
-            optional(one_of("+-".bytes())),
-            byte(b'0').or((digits(), optional((byte(b'.'), digits()))).map(|_| b'0')),
-            optional((
-                (one_of("eE".bytes()), optional(one_of("+-".bytes()))),
-                digits(),
-            )),
-        ))))
-        .map(|s: &'a [u8]| std::str::from_utf8(s).unwrap().parse().unwrap())
-        .expected("number"),
+    let field = (json_string(), lex(byte(b':')), json_value_()).map(|t| (t.0, t.2));
+    let fields = sep_by(field, lex(byte(b',')));
+    between(lex(byte(b'{')), lex(byte(b'}')), fields).expected("object")
+}
+
+fn array<'a, I>() -> impl Parser<Input = I, Output = Vec<Value<&'a str>>>
+where
+    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
+    I::Error: ParseError<I::Item, I::Range, I::Position>,
+{
+    between(
+        lex(byte(b'[')),
+        lex(byte(b']')),
+        sep_by(json_value_(), lex(byte(b','))),
     )
+    .expected("array")
 }
 
 fn json_string<'a, I>() -> impl Parser<Input = I, Output = &'a str>
@@ -98,55 +107,46 @@ where
     between(byte(b'"'), lex(byte(b'"')), inner).expected("string")
 }
 
-fn object<'a, I>() -> impl Parser<Input = I, Output = HashMap<&'a str, Value<&'a str>>>
+fn number<'a, I>() -> impl Parser<Input = I, Output = f64> + 'a
 where
     I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    let field = (json_string(), lex(byte(b':')), json_value_()).map(|t| (t.0, t.2));
-    let fields = sep_by(field, lex(byte(b',')));
-    between(lex(byte(b'{')), lex(byte(b'}')), fields).expected("object")
-}
-
-fn array<'a, I>() -> impl Parser<Input = I, Output = Vec<Value<&'a str>>>
-where
-    I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    between(
-        lex(byte(b'[')),
-        lex(byte(b']')),
-        sep_by(json_value_(), lex(byte(b','))),
+    no_partial(
+        lex(range::recognize(no_partial((
+            optional(one_of("+-".bytes())),
+            byte(b'0').or((digits(), optional((byte(b'.'), digits()))).map(|_| b'0')),
+            optional((
+                (one_of("eE".bytes()), optional(one_of("+-".bytes()))),
+                digits(),
+            )),
+        ))))
+        .map(|s: &'a [u8]| std::str::from_utf8(s).unwrap().parse().unwrap())
+        .expected("number"),
     )
-    .expected("array")
 }
 
-#[inline(always)]
-fn json_value<'a, I>() -> impl Parser<Input = I, Output = Value<&'a str>> + 'a
+fn digits<'a, I>() -> impl Parser<Input = I, Output = &'a [u8]> + 'a
 where
     I: RangeStream<Item = u8, Range = &'a [u8]> + 'a,
     I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    spaces().with(json_value_())
+    range::take_while1(|b| b >= b'0' && b <= b'9')
 }
 
-// We need to use `parser!` to break the recursive use of `value` to prevent the returned parser
-// from containing itself
-parser! {
-    #[inline(always)]
-    fn json_value_['a, I]()(I) -> Value<&'a str>
-        where [ I: RangeStream<Item = u8, Range = &'a [u8]> + 'a ]
-    {
-        choice((
-            json_string().map(Value::String),
-            object().map(Value::Object),
-            array().map(Value::Array),
-            number().map(Value::Number),
-            lex(range::range(&b"false"[..]).map(|_| Value::Bool(false))),
-            lex(range::range(&b"true"[..]).map(|_| Value::Bool(true))),
-            lex(range::range(&b"null"[..]).map(|_| Value::Null)),
-        ))
-    }
+fn lex<'a, P>(p: P) -> impl Parser<Input = P::Input, Output = P::Output>
+where
+    P: Parser,
+    P::Input: RangeStream<Item = u8, Range = &'a [u8]>,
+    <P::Input as StreamOnce>::Error: ParseError<
+        <P::Input as StreamOnce>::Item,
+        <P::Input as StreamOnce>::Range,
+        <P::Input as StreamOnce>::Position,
+    >,
+{
+    no_partial(p.skip(range::take_while(|b| {
+        b == b' ' || b == b'\t' || b == b'\r' || b == b'\n'
+    })))
 }
 
 fn main() {
