5eb964a1c7a445ce07c2e1582cb0c854e5057f5a
Ed Page <eopage@gmail.com>
2023-07-13 16:22:32 -0500

    chore(winnow): Upgrade to 0.5.0

--- examples/winnow-app/app.rs
+++ examples/winnow-app/app.rs
@@ -3,16 +3,16 @@ mod parser;
 
 use std::{env, fs};
 
-use winnow::error::VerboseError;
+use winnow::error::ContextError;
 use winnow::prelude::*;
 
 fn main() {
     let src = fs::read_to_string(env::args().nth(1).expect("Expected file argument"))
         .expect("Failed to read file");
 
-    match parser::json::<VerboseError<parser::Stream<'_>>>
+    match parser::json::<ContextError>
         .parse(src.as_str())
-        .map_err(VerboseError::into_owned)
+        .map_err(|e| e.to_string())
     {
         Ok(json) => {
             println!("{:#?}", json);
