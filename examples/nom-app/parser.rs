0fa402b6637811931ac9d4e3a184a80f61c84e74
Ed Page <eopage@gmail.com>
2025-01-27 11:06:53 -0600

    chore(nom): Remove example comments

--- examples/nom-app/parser.rs
+++ examples/nom-app/parser.rs
@@ -22,7 +22,6 @@ pub enum JsonValue {
     Object(HashMap<String, JsonValue>),
 }
 
-/// the root element of a JSON parser is either an object or an array
 pub fn root<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, JsonValue, E> {
@@ -37,7 +36,6 @@ pub fn root<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     )(i)
 }
 
-/// here, we apply the space parser before trying to parse a value
 fn json_value<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, JsonValue, E> {
@@ -87,10 +85,6 @@ fn key_value<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     )(i)
 }
 
-/// some combinators, like `separated_list0` or `many0`, will call a parser repeatedly,
-/// accumulating results in a `Vec`, until it encounters an error.
-/// If you want more control on the parser application, check out the `iterator`
-/// combinator (cf `examples/iterator.rs`)
 fn array<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, Vec<JsonValue>, E> {
@@ -106,25 +100,11 @@ fn array<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     )(i)
 }
 
-/// `tag(string)` generates a parser that recognizes the argument string.
-///
-/// we can combine it with other functions, like `value` that takes another
-/// parser, and if that parser returns without an error, returns a given
-/// constant value.
-///
-/// `alt` is another combinator that tries multiple parsers one by one, until
-/// one of them succeeds
 fn boolean<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, bool, E> {
-    // This is a parser that returns `true` if it sees the string "true", and
-    // an error otherwise
     let parse_true = value(true, tag("true"));
 
-    // This is a parser that returns `false` if it sees the string "false", and
-    // an error otherwise
     let parse_false = value(false, tag("false"));
 
-    // `alt` combines the two parsers. It returns the result of the first
-    // successful parser, or an error
     alt((parse_true, parse_false))(input)
 }
 
@@ -132,47 +112,16 @@ fn null<'a, E: ParseError<&'a str>>(input: &'a str) -> IResult<&'a str, (), E> {
     value((), tag("null"))(input)
 }
 
-/// A nom parser has the following signature:
-/// `Input -> IResult<Input, Output, Error>`, with `IResult` defined as:
-/// `type IResult<I, O, E = (I, ErrorKind)> = Result<(I, O), Err<E>>;`
-///
-/// most of the times you can ignore the error type and use the default (but this
-/// examples shows custom error types later on!)
-///
-/// Here we use `&str` as input type, but nom parsers can be generic over
-/// the input type, and work directly with `&[u8]` or any other type that
-/// implements the required traits.
-///
-/// Finally, we can see here that the input and output type are both `&str`
-/// with the same lifetime tag. This means that the produced value is a subslice
-/// of the input data. and there is no allocation needed. This is the main idea
-/// behind nom's performance.
 fn parse_str<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     escaped(alphanumeric, '\\', one_of("\"n\\"))(i)
 }
 
-/// parser combinators are constructed from the bottom up:
-/// first we write parsers for the smallest elements (here a space character),
-/// then we'll combine them in larger parsers
 fn sp<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, &'a str, E> {
     let chars = " \t\r\n";
 
-    // nom combinators like `take_while` return a function. That function is the
-    // parser,to which we can pass the input
     take_while(move |c| chars.contains(c))(i)
 }
 
-/// this parser combines the previous `parse_str` parser, that recognizes the
-/// interior of a string, with a parse to recognize the double quote character,
-/// before the string (using `preceded`) and after the string (using `terminated`).
-///
-/// `context` and `cut` are related to error management:
-/// - `cut` transforms an `Err::Error(e)` in `Err::Failure(e)`, signaling to
-/// combinators like  `alt` that they should not try other parsers. We were in the
-/// right branch (since we found the `"` character) but encountered an error when
-/// parsing the string
-/// - `context` lets you add a static string to provide more information in the
-/// error chain (to indicate which parser had an error)
 fn string<'a, E: ParseError<&'a str> + ContextError<&'a str>>(
     i: &'a str,
 ) -> IResult<&'a str, &'a str, E> {
