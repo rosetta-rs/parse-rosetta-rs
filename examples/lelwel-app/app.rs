92cd712e61595d8a070fb02d63c1595fd2ad92dd
Ed Page <eopage@gmail.com>
2025-05-01 11:02:19 -0500

    Upgrade lelwel to 0.8.0

--- examples/lelwel-app/app.rs
+++ examples/lelwel-app/app.rs
@@ -1,3 +1,4 @@
+mod lexer;
 mod parser;
 
 use std::{env, fs};
@@ -6,15 +7,13 @@ use codespan_reporting::diagnostic::Severity;
 use codespan_reporting::files::SimpleFile;
 use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 use codespan_reporting::term::{self, Config};
-use logos::Logos as _;
 
 fn main() {
     let path = env::args().nth(1).expect("Expected file argument");
     let src = fs::read_to_string(&path).expect("Failed to read file");
 
     let mut diags = vec![];
-    let (tokens, ranges) = parser::tokenize(parser::Token::lexer(&src), &mut diags);
-    let cst = parser::Parser::parse(&src, tokens, ranges, &mut diags);
+    let cst = parser::Parser::parse(&src, &mut diags);
 
     #[cfg(debug_assertions)]
     {
