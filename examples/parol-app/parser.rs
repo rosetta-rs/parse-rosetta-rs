f526552130baf0eac459f781da10086f7d1fe186
Ed Page <eopage@gmail.com>
2026-07-01 13:56:06 -0500

    test: Update snapshot

--- examples/parol-app/parser.rs
+++ examples/parol-app/parser.rs
@@ -58,6 +58,7 @@ scanner! {
 }
 
 const MAX_K: usize = 1;
+pub const SKIP_TOKENS_BY_SCANNER_STATE: &[&[parol_runtime::TerminalIndex]; 1] = &[&[]];
 
 pub const NON_TERMINALS: &[&str; 11] = &[
     /*  0 */ "Array",
@@ -307,12 +308,13 @@ where
     let mut user_actions = GrammarAuto::new(user_actions);
     llk_parser.parse_into(
         tree_builder,
-        TokenStream::new(
+        TokenStream::new_with_skip_tokens(
             input,
             file_name,
             scanner.scanner_impl.clone(),
             &GrammarScanner::match_function,
             MAX_K,
+            SKIP_TOKENS_BY_SCANNER_STATE,
         )
         .unwrap(),
         &mut user_actions,
